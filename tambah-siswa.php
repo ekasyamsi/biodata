69d4d066a3575702b2c32f374cb948d170e8235c
EKA SYAMSI <eka.syamsi@student.rosma.ac.id>
2025-11-21 13:21:21 +0700

    Add files via upload

---
+++ tambah-siswa.php
@@ -0,0 +1,60 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+    <title>Tambah Mahasiswa</title>
+  </head>
+
+  <body>
+
+    <div class="container" style="margin-top: 80px">
+      <div class="row">
+        <div class="col-md-8 offset-md-2">
+          <div class="card">
+            <div class="card-header">
+              TAMBAH MAHASISWA
+            </div>
+            <div class="card-body">
+              <form action="simpan-siswa.php" method="POST">
+                
+                <div class="form-group">
+                  <label>NIM</label>
+                  <input type="text" name="nim" placeholder="Masukkan NIM" class="form-control">
+                </div>
+
+                <div class="form-group">
+                  <label>Nama Lengkap</label>
+                  <input type="text" name="nama_lengkap" placeholder="Masukkan Nama Mahasiswa" class="form-control">
+                </div>
+
+                <div class="form-group">
+                  <label>Tempat Lahir</label>
+                  <input type="text" name="tempat_lahir" placeholder="Masukkan Tempat Lahir" class="form-control">
+                </div>
+
+                 <div class="form-group">
+                  <label>Tanggal Lahir</label>
+                  <input type="date" name="tanggal_lahir" placeholder="Masukkan Tanggal Lahir" class="form-control">
+                </div>
+
+                <div class="form-group">
+                  <label>Alamat</label>
+                  <textarea class="form-control" name="alamat" placeholder="Masukkan Alamat" rows="4"></textarea>
+                </div>
+                
+                <button type="submit" class="btn btn-success">SIMPAN</button>
+                <button type="reset" class="btn btn-warning">RESET</button>
+
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+  </body>
+</html>
